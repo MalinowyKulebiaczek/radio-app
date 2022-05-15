3c1c03d29365bd4bcfc0460750e5c92e71410a67
Rafał Wiercioch <rafa.wiercioch@gmail.com>
2022-05-15 20:28:06 +0200

    changed colours in drawer

--- routes/drawer.js
+++ routes/drawer.js
@@ -12,29 +12,38 @@ const Drawer = createDrawerNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-          
-      <Drawer.Group screenOptions={{
-                        headerStyle: { backgroundColor: COLORS.raDarker },
-                        // headerTitleStyle: {
-                        //     fontFamily: 'roboto-bold',
-                        //   },
-                        headerTintColor: COLORS.lightWhite,
-                        height: 80,
-                        headerTitleAlign: 'center',
-                        backgroundColor: COLORS.raDarker
-                    }}
-                    
-                    >
-        <Drawer.Screen name="Home" component={PlayScreenStack} options={{ title: 'Player', headerShown: false }} />
-        <Drawer.Screen name="RecordingStack" component={RecordingStack} 
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{
+        drawerStyle: {
+          backgroundColor: COLORS.raDarker,
+          shadowColor: COLORS.lightWhite,
+        },
+        drawerLabelStyle:{
+          color: COLORS.lightWhite
+        },
+        drawerActiveBackgroundColor: COLORS.raDark
+      }}        
+      >
+        <Drawer.Group screenOptions={{
+          headerStyle: { backgroundColor: COLORS.raDarker },
+          // headerTitleStyle: {
+          //     fontFamily: 'roboto-bold',
+          //   },
+          headerTintColor: COLORS.lightWhite,
+          height: 80,
+          headerTitleAlign: 'center',
+          backgroundColor: COLORS.raDarker
+        }}
+
+        >
+          <Drawer.Screen name="Home" component={PlayScreenStack} options={{ title: 'Player', headerShown: false }} />
+          <Drawer.Screen name="RecordingStack" component={RecordingStack}
             options={{ title: 'Nagrania', headerShown: false }} />
-        <Drawer.Screen name="AlbumOfTheWeekStack" component={AlbumOfTheWeekStack} 
-            options={{ title: 'Płyta tygodnia', headerShown: false  }} />
-        <Drawer.Screen name="Info" component={InfoStack} options={{ title: 'Informacje', headerShown: false}} />
+          <Drawer.Screen name="AlbumOfTheWeekStack" component={AlbumOfTheWeekStack}
+            options={{ title: 'Płyta tygodnia', headerShown: false }} />
+          <Drawer.Screen name="Info" component={InfoStack} options={{ title: 'Informacje', headerShown: false }} />
         </Drawer.Group>
       </Drawer.Navigator>
-      
+
     </NavigationContainer>
   );
 }
